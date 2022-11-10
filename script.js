1bb7a0e90d5518451a5b07e3166daa0da1ec88bf
Selena <selena.k.rodriguez@gmail.com>
2022-11-10 14:51:36 -0600

    Saving code prior to restarting on calculator.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const clear = document.getElementById("clear");
 const remove = document.getElementById("delete");
 const equals = document.getElementById("equals");
 let input = document.getElementById("input");
+let history = document.getElementById("history");
 let num1, num2, op;
 
 // if e.type is 'click' or if e.type is 'keydown' run the appropriate function
@@ -27,29 +28,32 @@ function clicked(e) {
     const number = e.target.innerHTML;
     console.log(number);
     if(firstInput) {
-        if(digit.includes(number)) {
+        if(keys) {
             input.innerHTML = number;
+            history.innerHTML = number;
             firstInput = false;
         } 
     } else {
-        if((digit.includes(number))) {
+        if(keys) {
             input.innerHTML += number;
+            history.innerHTML +=number;
         } 
     }
-    if(number === clear) {
-        input.innerHTML = "0";
+    if(number === "C") {
+        clearScreen();
     }
-    if(number === remove) {
-        input.innerHTML = input.innerHTML.substring(0, input.innerHTML.length -1)
+    if(number === "Del") {
+        deleteInput();
     }
     if(number === "+") {
-        num1 = input.innerHTML;
-        input.innerHTML += "+";
+        num1 = input.innerHTML.substring(0, input.innerHTML.length -1);
+        // input.innerHTML += "+";
         op = "+";
     }
     if(number === "=") {
-        num2 = input.innerHTML.replace(num1, "").replace(op, "");
+        num2 = input.innerHTML.replace(num1, "").replace(op, "").replace("=", "");
         input.innerHTML = compute(num1, num2);
+        history.innerHTML += input.innerHTML;
     }
     console.log("num1= " + num1);
     console.log("num2= " + num2);
@@ -64,7 +68,6 @@ function typed(e) {
     const number = e.key;
     const button = document.querySelector(`button[data-key="${e.key}"]`);
     console.log(number);
-    console.log(button);
     if (!button) {
         return;
     } else if (number === "c") {
@@ -84,6 +87,7 @@ function typed(e) {
 // zero's out screen and restarts the firstInput
 function clearScreen() {
     input.innerHTML = "0";
+    history.innerHTML = "0";
     firstInput = true;
 };
 
@@ -93,12 +97,14 @@ function deleteInput() {
     clearScreen();
   } else {
     input.innerHTML = input.innerHTML.substring(0, input.innerHTML.length -1);
+    history.innerHTML = history.innerHTML.substring(0, history.innerHTML.length -1);
   }
 };
 
 // limits the inputs to 6 characters
 function charLimit() {
     return input.innerHTML.substring(0,6);
+    return history.innerHTML.substring(0,6);
 };
 
 // addition
