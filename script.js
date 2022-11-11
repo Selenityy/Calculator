bbd822aaa3283ef162eae2a13078c12dff7a241c
Selena <selena.k.rodriguez@gmail.com>
2022-11-11 16:47:19 -0600

    Code 2.0, took out evaulation from data validation. Cleaned up the code with num1, num2. Added comments

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let firstInput = true;
+let hasEvaluated = false;
+const isDigitRegex = new RegExp(/(\d|\.)/);
+const isOperatorRegex = new RegExp(/[\+\-\÷\×\%\/\*]/g);
+const isEqualsRegex = new RegExp(/=/)
 const keys = document.querySelectorAll(".key");
 const digit = document.querySelectorAll(".digits");
 const operator = document.querySelectorAll(".operator");
@@ -10,26 +14,15 @@ let input = document.getElementById("input");
 let history = document.getElementById("history");
 let num1, num2, op;
 
-// if e.type is 'click' or if e.type is 'keydown' run the appropriate function
-function userInput(e) {
-    if (e.type === "click") {
-        clicked(e);
-    } else if (e.type === "keydown") {
-        typed(e);
-    } else {
-        return;
-    }
-}
-
 // changes the screen input for clicking a button
-keys.forEach(key => key.addEventListener('click', userInput));
+keys.forEach(key => key.addEventListener('click', clicked));
 
+// changes the screen input for pressing a key
+window.addEventListener('keydown', clicked);
+
+// data validation for the user input
 function clicked(e) {
-    console.log(e.type);
-    const number = e.target.innerHTML;
-    console.log(number);
-    const isDigitRegex = new RegExp(/(\d|\.)/);
-    const isOperatorRegex = new RegExp(/[\+\−\÷\×\%]/);
+    const number = e.type === "click" ? e.target.innerHTML : e.key;
     let isDigit = isDigitRegex.test(number);
     let isOperator = isOperatorRegex.test(number);
     if (firstInput) {
@@ -42,91 +35,77 @@ function clicked(e) {
         }
     } else {
         if (isDigit) {
-            input.innerHTML = number;
-            history.innerHTML +=number;
+            if (isEqualsRegex.test(history.innerHTML.slice(-1))) {
+                input.innerHTML = number;
+                history.innerHTML = number;
+                hasEvaluated = false;
+            } else if (!(isOperatorRegex.test(history.innerHTML.slice(-1)))) {
+                input.innerHTML += number;
+                history.innerHTML += number;
+            } else {
+                input.innerHTML = number;
+                history.innerHTML +=number;
+            }
         } else if (isOperator) {
             if (isOperatorRegex.test(history.innerHTML.slice(-1))) {
                 return;
+            } else if (isEqualsRegex.test(history.innerHTML.slice(-1))) {
+                history.innerHTML = input.innerHTML + number;
             } else {
-                console.log("mew")
-                history.innerHTML +=number;
+                history.innerHTML += number;
             }
         } 
     }
-    if (number === "C") {
+    if (number === "C" || number === "c") {
         clearScreen();
     }
-    if (number === "Del") {
-        deleteInput();
-    }
-    if (number === "+") {
-        num1 = Number(parseFloat(history.innerHTML.substring(0, history.innerHTML.length -1)));
-        op = "+";   
-    } else if (number === "−") {
-        num1 = Number(parseFloat(history.innerHTML.substring(0, history.innerHTML.length -1)));
-        op = "−";       
-    } else if (number === "×") {
-        num1 = Number(parseFloat(history.innerHTML.substring(0, history.innerHTML.length -1)));
-        op = "×";      
-    } else if (number === "÷") {
-        num1 = Number(parseFloat(history.innerHTML.substring(0, history.innerHTML.length -1)));
-        op = "÷";    
+    if (number === "Del" || number === "Backspace") {
+        deleteInput();  
     } else if (number === "%") {
         percent();
     }
-    if (number === "=") {
+    if (number === "=" || number === "Enter") {
+        e.preventDefault();
+        history.innerHTML += "=";
         evaluate();
     }
-    console.log("num1= " + num1);
-    console.log("num2= " + num2);
-    console.log("operator= " + op);
 };
-
-
-// changes the screen input for pressing a key
-window.addEventListener('keydown', userInput);
-
-function typed(e) {
-    console.log(e.type);
-    const number = e.key;
-    const button = document.querySelector(`button[data-key="${e.key}"]`);
-    console.log(number);
-    if (!button) {
-        return;
-    } else if (number === "c") {
-        clearScreen();
-    } else if (number === "Backspace") {
-        deleteInput();
-    } else if(firstInput) {
-        input.innerHTML = number;
-        firstInput = false;
-    } else {
-        input.innerHTML += number;
-    }
-}
 
 // zero's out screen and restarts the firstInput
 function clearScreen() {
     input.innerHTML = "0";
     history.innerHTML = "0";
+    num1 = "";
+    num2 = "";
+    op = "";
     firstInput = true;
+    hasEvaluated = false;
 };
 
 // deletes the last digit entered, and if it's the very last then it restarts at cleared
 function deleteInput() {
-  if(input.innerHTML.length === 1) {
+  if((input.innerHTML.length === 1)) {
     clearScreen();
+  } else if (isEqualsRegex.test(history.innerHTML.slice(-1))) {
+    return;
   } else {
     input.innerHTML = input.innerHTML.substring(0, input.innerHTML.length -1);
     history.innerHTML = history.innerHTML.substring(0, history.innerHTML.length -1);
   }
 };
 
-// labels num2 after clicking equals, uses the compute function to give final input answer
+// computes whether it has before or not
 function evaluate() {
-    num2 = Number(parseFloat(history.innerHTML.replace(num1, "").replace(op, "").replace("=", "")));
+    op = history.innerHTML.match(isOperatorRegex).pop();
+    if(!hasEvaluated) {
+        num1 = Number(parseFloat(history.innerHTML.substring(0, history.innerHTML.indexOf(op))));
+        num2 = Number(parseFloat(history.innerHTML.substring(history.innerHTML.indexOf(op) +1, history.innerHTML.indexOf("="))));
+    } else {
+        num2 = Number(parseFloat(history.innerHTML.substring(history.innerHTML.lastIndexOf(op) +1, history.innerHTML.indexOf("="))));
+    }
     input.innerHTML = Number(compute(op).toFixed(2));
-    history.innerHTML += "=" + input.innerHTML;
+    num1 = Number(parseFloat(input.innerHTML));
+    hasEvaluated = true;
 }
 
 // runs the actual computing functions and returns to evaluate function
@@ -134,11 +113,15 @@ function compute(op) {
     switch (op) {
         case "×":
             return multiply();
+        case "*":
+            return multiply();
         case "÷":
+            return divide();
+        case "/":
             return divide();
         case "+":
             return add();
-        case "−":
+        case "-":
             return subtract();
     }
 };
